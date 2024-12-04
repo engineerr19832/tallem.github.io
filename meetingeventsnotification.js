84ee6af105075ff3bf1b38572034e5592201ec76
talleem <engineerr19832@gmail.com>
2024-12-04 18:24:13 +0300

    Update meetingeventsnotification.js

--- meetingeventsnotification.js
+++ meetingeventsnotification.js
@@ -41,10 +41,10 @@ function loadMeetings() {
 }
         
             const checkNotificationTiming = setInterval(() => {
-                console.log('Interval running for event:', event.id);
+                console.log("Interval running for event:", event.id);
                 const now = new Date();
                 const timeBeforeStart = (eventStart - now) / 1000 / 60; // Convert to minutes
-                console.log('Time before event start (minutes):', timeBeforeStart);
+                console.log("Time before event start (minutes):", timeBeforeStart);
 
 
                 // Check various conditions for notification timing
@@ -79,7 +79,7 @@ function loadMeetings() {
                 }
                  else if (timeBeforeStart < 0 && Math.ceil(timeBeforeStart) === -5) {
                     showNotification(event);
-                     console.log('Clearing interval for event:', event.id);
+                     console.log("Clearing interval for event:", event.id);
                     clearInterval(checkNotificationTiming); // Stop the loop after the last notification
                 }
             }, 60000); // Check every minute
