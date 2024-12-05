8739d661a9bcca5a73f0b0fd62924d3f041cb6ef
talleem <engineerr19832@gmail.com>
2024-12-05 18:08:55 +0300

    Update meetingeventsnotification.js

--- meetingeventsnotification.js
+++ meetingeventsnotification.js
@@ -38,7 +38,7 @@ function loadMeetings() {
             console.log("Event start dateTime:", event.start?.dateTime);
                
             const checkNotificationTiming = setInterval(() => {
-                 console.log("Interval running for event:", event.id);
+                 //console.log("Interval running for event:", event.id);
                 const now = new Date();
                 const timeBeforeStart = (eventStart - now) / 1000 / 60; // Convert to minutes
                 console.log("Time before event start (minutes):", timeBeforeStart);
