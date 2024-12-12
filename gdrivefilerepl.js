4af28435dc59b521acc39ee149dc17641a83593e
talleem <engineerr19832@gmail.com>
2024-12-12 22:59:44 +0300

    Update gdrivefilerepl.js

--- gdrivefilerepl.js
+++ gdrivefilerepl.js
@@ -68,7 +68,6 @@ firestore.collection('meetings_his_tbl')
         } else {
             statusCell.textContent = 'Not in Firestore';
             console.log('Owner from Google Drive:', owner); // Debugging ownerEmail
-              console.log('Firestore creatorEmail:', doc.data().creatorEmail); // Inside the loop
         }
     })
     .catch(error => console.error('Error checking Firestore:', error));
