7c65f0c91d0ee0b636208b10635e6150c86d6aaf
talleem <engineerr19832@gmail.com>
2024-12-17 21:00:20 +0300

    Update googlemeetpage_AR.js

--- googlemeetpage_AR.js
+++ googlemeetpage_AR.js
@@ -98,12 +98,16 @@ function translateGoogleMeetPage() {
     allElements.forEach(element => {
         if (element.textContent.trim() === 'Enter:Google Meet list of attendees emails with the') {
             element.textContent = 'أدخل قائمة بريد الكتروني لمستخدمي غرف اجتماعات غوغل';
-            element.style.fontSize = '14px';  // Set the font size to 18px
-        element.style.fontWeight = 'bold'; // Optional: Make it bold
+            element.style.fontSize = '14px';  // Adjust font size
+            element.style.fontWeight = 'bold';
         } else if (element.textContent.trim() === 'logged email in blue bold font (REQUIRED)') {
             element.textContent = 'مع البريد المسجل باللون الأزرق الغامق (مطلوب)';
-            element.style.fontSize = '14px';  // Set the font size to 18px
-            element.style.fontWeight = 'bold'; // Optional: Make it bold
+            element.style.fontSize = '14px';  // Adjust font size
+            element.style.fontWeight = 'bold';
+        } else if (element.textContent.trim() === 'Attendee Emails') {
+            element.textContent = 'البريد الالكتروني للمدعوين';
+            element.style.fontSize = '14px';  // Adjust font size
+            element.style.fontWeight = 'bold'; // Optional: Bold
         }
     });
 }
