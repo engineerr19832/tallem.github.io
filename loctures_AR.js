401b5a8491a3031137b5026ddfb8d47b09271f9c
talleem <engineerr19832@gmail.com>
2024-12-21 22:47:03 +0300

    Update loctures_AR.js

--- loctures_AR.js
+++ loctures_AR.js
@@ -5,15 +5,15 @@ function translatePageToArabic() {
     // Translate buttons
     document.getElementById('deleteVideo').textContent = 'حذف المحتوى';
     document.getElementById('deleteVideo').style.fontWeight = 'bold';
-    document.getElementById('deleteVideo').style.fontSize = '1em';
+    document.getElementById('deleteVideo').style.fontSize = '0.8em';
     
     document.getElementById('fileupload').textContent = 'تحميل المحتوى';
     document.getElementById('fileupload').style.fontWeight = 'bold';
-    document.getElementById('fileupload').style.fontSize = '1em';
+    document.getElementById('fileupload').style.fontSize = '0.8em';
     
     document.getElementById('updatenotes').textContent = 'تعديل الملاحظات';
     document.getElementById('updatenotes').style.fontWeight = 'bold';
-    document.getElementById('updatenotes').style.fontSize = '1em';
+    document.getElementById('updatenotes').style.fontSize = '0.8em';
 
     console.log(document.getElementById('deleteVideo')); // Should log the button element or `null`
 console.log(document.getElementById('fileupload'));
