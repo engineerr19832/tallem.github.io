bc834cae0418b031ab50e7633d83a3980f935969
talleem <engineerr19832@gmail.com>
2024-12-21 22:40:32 +0300

    Update loctures_AR.js

--- loctures_AR.js
+++ loctures_AR.js
@@ -1,9 +1,6 @@
 function translatePageToArabic() {
     // Translate table headers
     document.querySelector('th:nth-child(1)').textContent = 'ايميل الاستاذ';
-    document.querySelector('th:nth-child(2)').textContent = 'تاريخ التحميل';
-    document.querySelector('th:nth-child(3)').textContent = 'عرض المحتوى';
-    document.querySelector('th:nth-child(4)').textContent = 'ملاحظات';
 
     // Translate buttons
     document.getElementById('deleteVideo').textContent = 'حذف المحتوى';
