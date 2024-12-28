fa767e302b196ab8c94d4116f63df60f1559ea9b
talleem <engineerr19832@gmail.com>
2024-12-28 21:36:15 +0300

    Update refresh_token.js

--- refresh_token.js
+++ refresh_token.js
@@ -1,38 +1,46 @@
-function refresh_token(accessToken) {
+    function refresh_token(accessToken) {
     const tokenInfoUrl = 'https://www.googleapis.com/oauth2/v1/tokeninfo';
     const refreshTokenUrl = 'https://oauth2.googleapis.com/token';
 
     // Check token validity
-    const tokenInfoResponse = await fetch(`${tokenInfoUrl}?access_token=${accessToken}`);
-    if (tokenInfoResponse.ok) {
-        return accessToken; // Token is valid
-    }
+    return fetch(`${tokenInfoUrl}?access_token=${accessToken}`)
+        .then(tokenInfoResponse => {
+            if (tokenInfoResponse.ok) {
+                // Token is valid
+                return accessToken;
+            }
 
-    // Refresh the token
-    const refreshToken = localStorage.getItem('refreshToken');
-    const requestBody = {
-        client_id: '919212619443-d2ck4cv25sfhvvg5n1rj82ob81h56362.apps.googleusercontent.com',
-        client_secret: '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',
-        refresh_token: refreshToken,
-        grant_type: 'refresh_token',
-    };
+            // If token is invalid, refresh it
+            const refreshToken = localStorage.getItem('refreshToken');
+            const requestBody = {
+                client_id:'919212619443-d2ck4cv25sfhvvg5n1rj82ob81h56362.apps.googleusercontent.com',
+                client_secret:'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',
+                refresh_token: refreshToken,
+                grant_type: 'refresh_token',
+            };
 
-    const refreshResponse = await fetch(refreshTokenUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-    });
+            return fetch(refreshTokenUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestBody),
+            });
+        })
+        .then(refreshResponse => {
+            if (refreshResponse.ok) {
+                return refreshResponse.json().then(data => {
+                    const newAccessToken = data.access_token;
 
-    if (refreshResponse.ok) {
-        const data = await refreshResponse.json();
-        const newAccessToken = data.access_token;
-
-        // Store the new access token
-        localStorage.setItem('accessToken', newAccessToken);
-        return newAccessToken;
-    }
-
-    throw new Error('Unable to refresh token.');
+                    // Store the new access token
+                    localStorage.setItem('accessToken', newAccessToken);
+                    return newAccessToken;
+                });
+            }
+            throw new Error('Unable to refresh token.');
+        })
+        .catch(error => {
+            console.error('Error refreshing token:', error);
+            throw error;
+        });
 }
