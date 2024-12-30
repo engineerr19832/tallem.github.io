611d6f7eecb708c70349aa223845e463b416a0f4
talleem <engineerr19832@gmail.com>
2024-12-30 23:47:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
     });
-
-    win.loadFile('index.html');  // Ensure this file exists in your project
-
-     // Open the DevTools.
-    win.webContents.openDevTools();
+    win.loadFile('index.html'); // Ensure this file exists
 }
-
 app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
