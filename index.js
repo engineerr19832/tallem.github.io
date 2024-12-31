11a6ddb4b42ebca2ed0e9f088def0efc76aa9272
talleem <engineerr19832@gmail.com>
2024-12-31 19:39:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { app, BrowserWindow } = require('electron');
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
     });
-    win.loadFile('index.html'); // Ensure this file exists
+
+    win.loadURL('data:text/html;charset=utf-8,<html><body><h1>Test Window</h1></body></html>');
 }
+
 app.whenReady().then(createWindow);
