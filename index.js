a13c98fdbcc2d495526ebe469a1c6df1954dc331
talleem <engineerr19832@gmail.com>
2025-01-01 19:10:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+//const { app, BrowserWindow } = require('electron');
+//function createWindow() {
+//    const win = new BrowserWindow({
+   //     width: 800,
+    //    height: 600,
+  //  });
+  //  win.loadURL('data:text/html;charset=utf-8,<html><body><h1>Test Window</h1></body></html>');
+//}
+
+
 const { app, BrowserWindow } = require('electron');
 
-function createWindow() {
+app.on('ready', () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
     });
+    win.loadURL('data:text/html;charset=utf-8,<h1>Hello, Electron!</h1>');
+});
 
-    win.loadURL('data:text/html;charset=utf-8,<html><body><h1>Test Window</h1></body></html>');
-}
-
-app.whenReady().then(createWindow);
-app.on('ready', () => console.log('App is ready'));
-app.on('window-all-closed', () => console.log('All windows closed'));
-app.on('activate', () => console.log('App activated'));
-app.on('error', (err) => console.error('App Error:', err));
+//app.whenReady().then(createWindow);
+//app.on('ready', () => console.log('App is ready'));
+//app.on('window-all-closed', () => console.log('All windows closed'));
+//app.on('activate', () => console.log('App activated'));
+//app.on('error', (err) => console.error('App Error:', err));
 
